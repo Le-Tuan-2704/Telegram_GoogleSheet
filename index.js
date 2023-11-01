b1e07b162212f573dc5d112c5cb957b4f50eda93
letuan2704 <leduytuan27042000@gmail.com>
2023-11-01 18:02:48 +0700

    tool export product

--- index.js
+++ index.js
@@ -1,4 +1,10 @@
 const ToolBalanceMaster = require('./src/toolBalanceMaster/index');
 
+const ToolExportProduct = require('./src/toolExportProduct/index');
+
 // start tool BalanceMaster
-ToolBalanceMaster.start();
+// ToolBalanceMaster.start();
+
+// start tool ExportProduct
+
+ToolExportProduct.start();
